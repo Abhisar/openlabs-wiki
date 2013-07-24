61223119183b27fb1e59df3e1cee15ecfdb7ca65
Abhisar <abhisar.mohapatra@openlabs.co.in>
2013-07-24 10:09:23 +0530

    made the changes #1927

--- openlabs_wiki/openlabs_wiki/urls.py
+++ openlabs_wiki/openlabs_wiki/urls.py
@@ -1,24 +1,22 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-
-   url('wiki/create/$','wiki.views.create_page'),
-   url('wiki/save/$','wiki.views.save'),
-   url(r'^wiki/(?P<page_id>[^/]+)/$','wiki.views.view_page'),
-   url(r'^wiki/(?P<page_id>[^/]+)/edit/$','wiki.views.edit_page'),
-   url(r'^wiki/(?P<page_id>[^/]+)/save_edit/$','wiki.views.save_edit'),
-
-    url('accounts/login/$','openlabs_wiki.views.login'),
-    url('accounts/logout/$','openlabs_wiki.views.logout'),
-    url('accounts/auth/$','openlabs_wiki.views.auth_view'),
-    url('accounts/loggedin/$','openlabs_wiki.views.loggedin'),
-    url('accounts/invalid/$','openlabs_wiki.views.invalid_login'),
-    url('accounts/register/$','openlabs_wiki.views.register_user'),
-    url('accounts/register_success/$','openlabs_wiki.views.register_success'),
+    url('wiki/create/$', 'wiki.views.create_page'),
+    url('wiki/save/$', 'wiki.views.save'),
+    url(r'^wiki/(?P<page_id>[^/]+)/$', 'wiki.views.view_page'),
+    url(r'^wiki/(?P<page_id>[^/]+)/edit/$', 'wiki.views.edit_page'),
+    url(r'^wiki/(?P<page_id>[^/]+)/save_edit/$', 'wiki.views.save_edit'),
+    url('accounts/login/$', 'openlabs_wiki.views.login'),
+    url('accounts/logout/$', 'openlabs_wiki.views.logout'),
+    url('accounts/auth/$', 'openlabs_wiki.views.auth_view'),
+    url('accounts/loggedin/$', 'openlabs_wiki.views.loggedin'),
+    url('accounts/invalid/$', 'openlabs_wiki.views.invalid_login'),
+    url('accounts/register/$', 'openlabs_wiki.views.register_user'),
+    url('accounts/register_success/$', 'openlabs_wiki.views.register_success'),
     # Examples:
     # url(r'^$', 'openlabs_wiki.views.home', name='home'),
     # url(r'^openlabs_wiki/', include('openlabs_wiki.foo.urls')),
--- openlabs_wiki/wiki/views.py
+++ openlabs_wiki/wiki/views.py
@@ -9,58 +9,79 @@ from django.shortcuts import render_to_response
 from django.template.loader import get_template
 from django.http import HttpResponseRedirect
 
+
 def create_page(request):
     """creates a new page if user is logged  """
     if request.user.is_authenticated():
-       content=""
-       return render_to_response("create.html",{"content":content},context_instance=RequestContext(request))
+        content = ""
+        return render_to_response(
+            "create.html", {"content": content},
+            context_instance=RequestContext(request)
+        )
     else:
-       return  HttpResponseRedirect('/accounts/login')
+        return HttpResponseRedirect('/accounts/login')
+
 
 def save(request):
     """saves the page created"""
-    title=request.POST["title"]
-    checkvalue=request.POST.get('check', False)
-    owner=request.user.username
+    title = request. POST["title"]
+    checkvalue = request.POST.get('check', False)
+    owner = request.user.username
     try:
-           page=Article.objects.get(title=title,user=owner)
-           return render_to_response("alreadyexists.html")
-    except Article.DoesNotExist: 
-           page=Article(title=title,user=owner,flag=checkvalue)
+        page = Article.objects.get(title=title, user=owner)
+        return render_to_response("alreadyexists.html")
+    except Article.DoesNotExist:
+        page = Article(title=title, user=owner, flag=checkvalue)
     page.save()
-    getpage=Article.objects.get(title=title,user=owner)
-    content=request.POST["content"]
-    savecontent=history(page_id=getpage,content=content,edited_by=owner)
+    getpage = Article.objects.get(title=title, user=owner)
+    content = request.POST["content"]
+    savecontent = history(page_id=getpage, content=content, edited_by=owner)
     savecontent.save()
     return HttpResponseRedirect("/wiki/"+str(getpage.id)+"/")
 
-def view_page(request,page_id):
+
+def view_page(request, page_id):
     """to view a page based on page_id"""
-    page=Article.objects.get(id=page_id)
-    page_title=page.title
-    page_content=history.objects.filter(page_id=page_id).latest('edited').content
-    return render_to_response("view.html",{"page_title":page_title,"content":page_content,"page_id":page_id})
+    page = Article.objects.get(id=page_id)
+    page_title = page.title
+    page_content = history.objects.filter(page_id=page_id) \
+        .latest('edited').content
+    return render_to_response(
+        "view.html", {
+            "page_title": page_title,
+            "content": page_content,
+            "page_id": page_id
+        }
+    )
 
 
-def edit_page(request,page_id):
+def edit_page(request, page_id):
     """function edits the existing page"""
-    page=Article.objects.get(id=page_id)
-    page_title=page.title
-    page_content=history.objects.filter(page_id=page_id).latest('edited').content
-    return render_to_response("edit.html",{"page_title":page_title,"content":page_content,"page_id":page_id},context_instance=RequestContext(request))
+    page = Article.objects.get(id=page_id)
+    page_title = page.title
+    page_content = history.objects.filter(page_id=page_id) \
+        .latest('edited').content
+    return render_to_response(
+        "edit.html", {
+            "page_title": page_title, "content": page_content,
+            "page_id": page_id
+            }, context_instance=RequestContext(request)
+    )
 
-def save_edit(request,page_id):
+
+def save_edit(request, page_id):
     """saves the page after edit"""
-    page=Article.objects.get(id=page_id)
-    page_title=page.title
-    page_content=request.POST["content"]
-    loggedin_user=request.user.username
-    newchange=history(page_id=page,content=page_content,edited_by=loggedin_user)
+    page = Article.objects.get(id=page_id)
+    page_title = page.title
+    page_content = request.POST["content"]
+    loggedin_user = request.user.username
+    newchange = history(
+        page_id=page, content=page_content, edited_by=loggedin_user
+    )
     newchange.save()
-    content=history.objects.filter(page_id=page_id).latest('edited').content
-    return render_to_response("view.html",{"page_title":page_title,"content":page_content,"page_id":page_id})
-    
-
-
-    
-
+    content = history.objects.filter(page_id=page_id).latest('edited').content
+    return render_to_response(
+        "view.html", {
+            "page_title": page_title, "content": content,
+            "page_id": page_id}
+    )
